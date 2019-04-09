68540333b8f993b91b6d2df269a9c818a33c89ba
Tarun Mahajan <2922986+tango4567@users.noreply.github.com>
2019-04-10 02:00:12 +0530

    Update tapping.ino

--- tapping.ino
+++ tapping.ino
@@ -9,14 +9,11 @@ void setup() {
 }
 
 void loop() {
-  for (pos = 0; pos <=155; pos += 1) { 
+  for (pos = 0; pos <=115; pos += 1) { 
     // in steps of 1 degree
     myservo.write(pos);             
-      delay(15);                  
   }
-  for (pos = 155; pos >= 0; pos -= 1) { 
-  
-    myservo.write(pos);             
-    delay(15);                 
+  for (pos = 110; pos >= 0; pos -= 1) { 
+    myservo.write(pos);           
   }
 }
